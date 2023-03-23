7e8510e6ebad6a10e21bfdc8273817d514742ed2
Ammar Daham <ammar.daham@metropolia.fi>
2023-03-23 15:03:41 +0200

    8.4: Books of an author

--- GraphQL_Server/index.js
+++ GraphQL_Server/index.js
@@ -130,7 +130,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -139,7 +139,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) {
+        return books;
+      }
+      return books.filter((book) => book.author === args.author);
+    },
     allAuthors: () =>  {
       return authors.map(author => ({
         name: author.name,
